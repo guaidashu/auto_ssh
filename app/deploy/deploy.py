0476619c818fefbd9c176d1ce1de0f173d5443af
yy <a1023767856@163.com>
2019-12-30 14:38:29 +0800

    finished deploy script

--- app/deploy/deploy.py
+++ app/deploy/deploy.py
@@ -3,18 +3,22 @@ Create by yy on 2019/12/25
 """
 import sys
 import getopt
+import threading
 
 import paramiko
 from tool_yy import debug, Thread
-from app.config import server_config
 
 __all__ = ["AutoDeploy"]
+
+lock = threading.RLock()
 
 
 class AutoDeploy(Thread):
     def __init__(self, init_db):
         super().__init__()
         self.db = init_db('AUTODEPLOY')
+        self.script_id = 1
+        self.cmd = self.get_cmd()
 
     def run(self):
         # 命令行参数
@@ -33,8 +37,9 @@ class AutoDeploy(Thread):
                 pk = arg
 
         # 从配置文件获取 服务器地址
-        server_list = server_config.server_list
-        self.start_thread(server_list, self.ssh_start, max_worker=5, is_test=False, pk=pk)
+        server_list = self.get_server_list()
+        results = self.start_thread(server_list, self.ssh_start, is_test=False, pk=pk)
+        # debug(results)
 
     def ssh_start(self, server, pk):
         # 创建ssh对象
@@ -59,26 +64,42 @@ class AutoDeploy(Thread):
             pass
 
         # 判断是否传入了
-        debug(server)
         if pk == "":
             ssh.connect(hostname=server["host"], port=port, username=username)
         else:
             ssh.connect(hostname=server["host"], port=port, username=username, key_filename=pk)
 
-        cmd = self.get_cmd()
-
-        stdin, stdout, stderr = ssh.exec_command(cmd)
+        stdin, stdout, stderr = ssh.exec_command(self.cmd)
         result = stdout.read()
 
         if not result:
             result = stderr.read()
         ssh.close()
 
-        debug(result.decode())
+        return result
 
     def get_cmd(self):
         """
         获取要执行的命令
         :return:
         """
-        return "ls"
+        lock.acquire()
+        data = self.db.select({
+            "table": "script",
+            "condition": ["id={id}".format(id=self.script_id)]
+        }, is_close_db=False, get_all=False)
+        lock.release()
+        try:
+            data = data["content"]
+        except:
+            data = "echo cmd is null"
+        return data
+
+    def get_server_list(self):
+        lock.acquire()
+        server_list = self.db.select({
+            "table": "server_list",
+            "condition": ["is_execute=1", "and", "status=1"]
+        }, is_close_db=False)
+        lock.release()
+        return server_list
